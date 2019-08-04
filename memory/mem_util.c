90d625bc3757f46191cd86d8f95c6831a7959014
dejavudwh <494319000@qq.com>
2019-08-04 11:19:54 +0800

    Add allocate 4k memory function to reduce memory fragmentation

--- memory/mem_util.c
+++ memory/mem_util.c
@@ -84,3 +84,17 @@ int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size) {
 
     return -1;
 }
+
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size) {
+    unsigned int a;
+    size = (size + 0xfff) & 0xfffff000;
+    a = memman_alloc(man, size);
+    return a;
+}
+
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size) {
+    int i ;
+    size = (size + 0xfff) & 0xfffff000;
+    i = memman_free(man, addr, size);
+    return i;
+}
--- memory/mem_util.h
+++ memory/mem_util.h
@@ -16,3 +16,7 @@ unsigned int memman_total(struct MEMMAN *man);
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
 
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
+unsigned int memman_allock_4k(struct MEMMAN *man, unsigned int size);
+
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
